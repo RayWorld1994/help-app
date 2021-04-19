0967a2931f207f038c2498095bce6764b7e46dad
Erick Raymundo <egarcia@applaudostudios.com>
2021-04-18 20:40:59 -0600

    🚀 fixing deploy

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/help-app'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/help-app' }
-  );
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/help-app/'}),
+);
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
